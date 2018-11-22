072e2686d0534442d3aa5567e679e34a75dcfb77
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-11-22 03:20:03 -0600

    a

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.render('index');
 });
 
 app.listen(7000, function(){
