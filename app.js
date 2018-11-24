b0a148be7f1804a25f85877248f4d55f53408cb6
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-11-24 00:06:30 -0600

    added bootstrap

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost/zApp');
+
 let db = mongoose.connection;
 
 db.once('open', function(){
@@ -19,6 +20,14 @@ let Article = require('./models/articles');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
   Article.find({}, function(err, articles){
@@ -37,6 +46,22 @@ app.get('/articles/add', function (req, res) {
   res.render('add_articles', {
     title: 'Add Articles'
   });
+});
+
+app.post('/articles/add', function(req,res){
+let article = new Article();
+article.title = req.body.title;
+article.author = req.body.author;
+article.body = req.body.body;
+
+article.save(function(err){
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    res.redirect('/');
+  }
+});
 });
 
 app.listen(7000, function(){
