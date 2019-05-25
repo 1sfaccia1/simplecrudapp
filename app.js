cf275dacf9d6c3a7d22f1132064dfd810a5bea3a
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-25 17:47:22 -0500

    add read me

--- app.js
+++ app.js
@@ -8,19 +8,18 @@ const session = require('express-session');
 const passport = require('passport');
 const config = require('./config/database');
 
-
 mongoose.connect('mongodb://localhost/zApp', {
-  useNewUrlParser: true
+	useNewUrlParser: true
 });
 
 let db = mongoose.connection;
 
 db.once('open', function() {
-  console.log('Connected to MongoDB');
+	console.log('Connected to MongoDB');
 });
 
 db.on('error', function() {
-  console.log(err);
+	console.log(err);
 });
 
 const app = express();
@@ -28,50 +27,54 @@ const app = express();
 let Article = require('./models/article');
 let Linkup = require('./models/linkup');
 
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 
 // parse application/json
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: true,
+		saveUninitialized: true
+	})
+);
 
 app.use(require('connect-flash')());
 app.use(function(req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
+	res.locals.messages = require('express-messages')(req, res);
+	next();
 });
 
 // Express Validator Middleware
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-    var namespace = param.split('.'),
-      root = namespace.shift(),
-      formParam = root;
+app.use(
+	expressValidator({
+		errorFormatter: function(param, msg, value) {
+			var namespace = param.split('.'),
+				root = namespace.shift(),
+				formParam = root;
 
-    while (namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param: formParam,
-      msg: msg,
-      value: value
-    };
-  }
-}));
+			while (namespace.length) {
+				formParam += '[' + namespace.shift() + ']';
+			}
+			return {
+				param: formParam,
+				msg: msg,
+				value: value
+			};
+		}
+	})
+);
 
 // Passport config
 require('./config/passport')(passport);
@@ -81,26 +84,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('*', function(req, res, next) {
-  res.locals.user = req.user || null;
-  next();
+	res.locals.user = req.user || null;
+	next();
 });
 
 app.get('/', function(req, res) {
-  Article.find({}, function(err, articles) {
-    Linkup.find({}, function(err, linkups) {
-
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index', {
-          title: 'Articles',
-          articles: articles,
-          title: 'Linkups',
-          linkups: linkups
-        });
-      }
-    });
-  });
+	Article.find({}, function(err, articles) {
+		Linkup.find({}, function(err, linkups) {
+			if (err) {
+				console.log(err);
+			} else {
+				res.render('index', {
+					title: 'Articles',
+					articles: articles,
+					title: 'Linkups',
+					linkups: linkups
+				});
+			}
+		});
+	});
 });
 
 // Route files
@@ -111,7 +113,6 @@ app.use('/articles', articles);
 app.use('/linkups', linkups);
 app.use('/users', users);
 
+const port = process.env.PORT || 5500;
 
-const PORT = process.env.PORT || 5500;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
